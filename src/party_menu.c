d96919741e35517002d5930e617d232de92a8b10
jsryan <jsryan95@hotmail.com>
2025-07-02 17:31:18 +0100

    Changed Professor Oak's aide in Pewter City to give the player HM01 after they defeat Brock. Made using Cut in the field possible without any badges.

--- src/party_menu.c
+++ src/party_menu.c
@@ -3923,7 +3923,8 @@ static void CursorCB_FieldMove(u8 taskId)
     else
     {
         // All field moves before WATERFALL are HMs.
-        if (fieldMove <= FIELD_MOVE_WATERFALL && FlagGet(FLAG_BADGE01_GET + fieldMove) != TRUE)
+        if (fieldMove <= FIELD_MOVE_WATERFALL && FlagGet(FLAG_BADGE01_GET + fieldMove) != TRUE
+                && fieldMove != FIELD_MOVE_CUT)
         {
             DisplayPartyMenuMessage(gText_CantUseUntilNewBadge, TRUE);
             gTasks[taskId].func = Task_ReturnToChooseMonAfterText;
