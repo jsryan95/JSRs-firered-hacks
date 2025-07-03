50c7f502dfdb81f8522e9afa7a74c816f00dc7c4
jsryan <jsryan95@hotmail.com>
2025-07-03 16:52:20 +0100

    Stopped Pickup being able to generate TM10 (Hidden Power).

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -778,7 +778,6 @@ static const struct PickupItem sPickupItems[] =
     { ITEM_RAWST_BERRY, 55 },
     { ITEM_ASPEAR_BERRY, 65 },
     { ITEM_PERSIM_BERRY, 75 },
-    { ITEM_TM10, 80 },
     { ITEM_PP_UP, 85 },
     { ITEM_RARE_CANDY, 90 },
     { ITEM_NUGGET, 95 },
