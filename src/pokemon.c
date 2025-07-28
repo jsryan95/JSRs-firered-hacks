6988cb6dfaf63d4becf1900e2f642ae724eb1547
jsryan <jsryan95@hotmail.com>
2025-07-28 22:39:38 +0100

    Replaced evolutions that aren't possible in FRLG single-player with ones that are: - Pokémon that evolve by being traded with an item now evolve by levelling up with the same item. - Feebas now evolves by levelling up with a Blue Scarf. - Eevee now evolves into Espeon or Umbreon using a Sun Stone or Moon Stone, respectively. - Pokémon that evolve by trading now evolve by level: Graveler at 38, Haunter and Machoke at 40, and Kadabra at 44.

--- src/pokemon.c
+++ src/pokemon.c
@@ -5111,6 +5111,14 @@ u16 GetEvolutionTargetSpecies(struct Pokemon *mon, u8 type, u16 evolutionItem)
                 if (gEvolutionTable[species][i].param <= level)
                     targetSpecies = gEvolutionTable[species][i].targetSpecies;
                 break;
+            case EVO_LEVEL_UP_ITEM:
+                if (gEvolutionTable[species][i].param == heldItem)
+                {
+                    heldItem = ITEM_NONE;
+                    SetMonData(mon, MON_DATA_HELD_ITEM, &heldItem);
+                    targetSpecies = gEvolutionTable[species][i].targetSpecies;
+                }
+                break;
             case EVO_LEVEL_ATK_GT_DEF:
                 if (gEvolutionTable[species][i].param <= level)
                     if (GetMonData(mon, MON_DATA_ATK, NULL) > GetMonData(mon, MON_DATA_DEF, NULL))
