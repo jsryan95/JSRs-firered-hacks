244333fb6e3f9694b1c82a00a046caebda48dfec
jsryan <jsryan95@hotmail.com>
2025-07-31 19:53:40 +0100

    Changed Focus Energy to give +3 crit rate instead of +2.

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -1424,7 +1424,7 @@ static void Cmd_critcalc(void)
 
     gPotentialItemEffectBattler = gBattlerAttacker;
 
-    critChance  = 2 * ((gBattleMons[gBattlerAttacker].status2 & STATUS2_FOCUS_ENERGY) != 0)
+    critChance  = 3 * ((gBattleMons[gBattlerAttacker].status2 & STATUS2_FOCUS_ENERGY) != 0)
                 + (gBattleMoves[gCurrentMove].effect == EFFECT_HIGH_CRITICAL)
                 + (gBattleMoves[gCurrentMove].effect == EFFECT_BLAZE_KICK)
                 + (gBattleMoves[gCurrentMove].effect == EFFECT_POISON_TAIL)
