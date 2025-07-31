64c9c747b8ebfafb3afe0a979ef1603247bd74ff
jsryan <jsryan95@hotmail.com>
2025-07-31 11:31:51 +0100

    Changed the wild Pokémon encounters for some early routes.

--- include/wild_encounter.h
+++ include/wild_encounter.h
@@ -3,7 +3,7 @@
 
 #include "global.h"
 
-#define LAND_WILD_COUNT     12
+#define LAND_WILD_COUNT     10
 #define WATER_WILD_COUNT    5
 #define ROCK_WILD_COUNT     5
 #define FISH_WILD_COUNT     10
--- src/wild_encounter.c
+++ src/wild_encounter.c
@@ -47,20 +47,20 @@ static void AddToWildEncounterRateBuff(u8 encouterRate);
 #include "data/wild_encounters.h"
 
 static const u8 sUnownLetterSlots[][LAND_WILD_COUNT] = {
-  //  A   A   A   A   A   A   A   A   A   A   A   ?
-    { 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 27},
-  //  C   C   C   D   D   D   H   H   H   U   U   O
-    { 2,  2,  2,  3,  3,  3,  7,  7,  7, 20, 20, 14},
-  //  N   N   N   N   S   S   S   S   I   I   E   E
-    {13, 13, 13, 13, 18, 18, 18, 18,  8,  8,  4,  4},
-  //  P   P   L   L   J   J   R   R   R   Q   Q   Q
-    {15, 15, 11, 11,  9,  9, 17, 17, 17, 16, 16, 16},
-  //  Y   Y   T   T   G   G   G   F   F   F   K   K
-    {24, 24, 19, 19,  6,  6,  6,  5,  5,  5, 10, 10},
-  //  V   V   V   W   W   W   X   X   M   M   B   B
-    {21, 21, 21, 22, 22, 22, 23, 23, 12, 12,  1,  1},
-  //  Z   Z   Z   Z   Z   Z   Z   Z   Z   Z   Z   !
-    {25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 26},
+  //  A   A   A   A   A   A   A   A   A   ?
+    { 0,  0,  0,  0,  0,  0,  0,  0,  0,  27},
+  //  C   C   C   D   D   H   H   U   U   O
+    { 2,  2,  2,  3,  3,  7,  7, 20, 20, 14},
+  //  N   N   N   S   S   S   I   I   E   E
+    {13, 13, 13, 18, 18, 18,  8,  8,  4,  4},
+  //  P   P   L   L   J   J   R   R   Q   Q
+    {15, 15, 11, 11,  9,  9, 17, 17, 16, 16},
+  //  Y   Y   T   T   G   G   F   F   K   K
+    {24, 24, 19, 19,  6,  6,  5,  5, 10, 10},
+  //  V   V   W   W   X   X   M   M   B   B
+    {21, 21, 22, 22, 23, 23, 12, 12,  1,  1},
+  //  Z   Z   Z   Z   Z   Z   Z   Z   Z   !
+    {25, 25, 25, 25, 25, 25, 25, 25, 25, 26},
 };
 
 void DisableWildEncounters(bool8 state)
@@ -92,10 +92,6 @@ static u8 ChooseWildMonIndex_Land(void)
         return 8;
     else if (rand >= ENCOUNTER_CHANCE_LAND_MONS_SLOT_8 && rand < ENCOUNTER_CHANCE_LAND_MONS_SLOT_9)
         return 9;
-    else if (rand >= ENCOUNTER_CHANCE_LAND_MONS_SLOT_9 && rand < ENCOUNTER_CHANCE_LAND_MONS_SLOT_10)
-        return 10;
-    else
-        return 11;
 }
 
 static u8 ChooseWildMonIndex_WaterRock(void)
