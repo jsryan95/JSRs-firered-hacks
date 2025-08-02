b015a1f0c9f4ea42f9f6a61511ed12e59a2aa5b3
jsryan <jsryan95@hotmail.com>
2025-08-02 21:06:55 +0100

    Reduced the delay after displaying messages in battle.

--- include/constants/battle.h
+++ include/constants/battle.h
@@ -322,9 +322,9 @@
 #define BATTLE_TERRAIN_LANCE       18
 #define BATTLE_TERRAIN_CHAMPION    19
 
-#define B_WAIT_TIME_LONG  64
-#define B_WAIT_TIME_MED   48
-#define B_WAIT_TIME_SHORT 32
+#define B_WAIT_TIME_LONG  36
+#define B_WAIT_TIME_MED   27
+#define B_WAIT_TIME_SHORT 18
 
 #define CASTFORM_NORMAL     0
 #define CASTFORM_FIRE       1
