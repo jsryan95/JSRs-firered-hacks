44952940d98c8001173ff9c3081f144fcc555718
jsryan <jsryan95@hotmail.com>
2025-08-03 09:47:39 +0100

    Reduced the duration of sleep to 1-4 turns.

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -2736,7 +2736,7 @@ void SetMoveEffect(bool8 primary, u8 certain)
             BattleScriptPush(gBattlescriptCurrInstr + 1);
 
             if (sStatusFlagsForMoveEffects[gBattleCommunication[MOVE_EFFECT_BYTE]] == STATUS1_SLEEP)
-                gBattleMons[gEffectBattler].status1 |= STATUS1_SLEEP_TURN((Random() & 3) + 2); // 2-5 turns
+                gBattleMons[gEffectBattler].status1 |= STATUS1_SLEEP_TURN((Random() & 3) + 1); // 1-4 turns
             else
                 gBattleMons[gEffectBattler].status1 |= sStatusFlagsForMoveEffects[gBattleCommunication[MOVE_EFFECT_BYTE]];
 
