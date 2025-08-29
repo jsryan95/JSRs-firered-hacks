6de8f889359d6d121922efa831b50c5447b93d93
jsryan <jsryan95@hotmail.com>
2025-08-29 18:59:18 +0100

    Revert "Make all Pokémon appear as seen in the Pokédex".

--- src/pokedex_screen.c
+++ src/pokedex_screen.c
@@ -2237,7 +2237,13 @@ s8 DexScreen_GetSetPokedexFlag(u16 nationalDexNo, u8 caseId, bool8 indexIsSpecie
     switch (caseId)
     {
     case FLAG_GET_SEEN:
-        retVal = 1;
+        if (gSaveBlock2Ptr->pokedex.seen[index] & mask)
+        {
+            // Anticheat
+            if ((gSaveBlock2Ptr->pokedex.seen[index] & mask) == (gSaveBlock1Ptr->seen1[index] & mask)
+                && (gSaveBlock2Ptr->pokedex.seen[index] & mask) == (gSaveBlock1Ptr->seen2[index] & mask))
+                retVal = 1;
+        }
         break;
     case FLAG_GET_CAUGHT:
         if (gSaveBlock2Ptr->pokedex.owned[index] & mask)
